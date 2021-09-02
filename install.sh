aa7a8e97de53253dba2e10d33464adcda137da56
Rafał Babski <rbabski@n7space.com>
2021-09-02 11:20:50 +0200

    Fixed warnings, rename of Common into RuntimeCommon

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ SOURCES=$(dirname $0)
 mkdir -p "${PREFIX}/include/TASTE-Runtime-Common/src"
 cp -r "${SOURCES}/src/Packetizer" "${PREFIX}/include/TASTE-Runtime-Common/src"
 cp -r "${SOURCES}/src/Broker" "${PREFIX}/include/TASTE-Runtime-Common/src"
-cp -r "${SOURCES}/src/Common" "${PREFIX}/include/TASTE-Runtime-Common/src"
+cp -r "${SOURCES}/src/RuntimeCommon" "${PREFIX}/include/TASTE-Runtime-Common/src"
--- src/RuntimeMocks/system_spec.c
+++ src/RuntimeMocks/system_spec.c
@@ -8,6 +8,6 @@ enum RemoteInterface bus_to_port_map[] = {
     INTERFACE_INVALID_ID,
 };
 
-enum SystemBus device_to_bus_map[SYSTEM_DEVICE_NUMBER] = {};
+enum SystemBus device_to_bus_map[SYSTEM_DEVICE_NUMBER] = { 0 };
 
-void* device_configurations[SYSTEM_DEVICE_NUMBER] = {};
+void* device_configurations[SYSTEM_DEVICE_NUMBER] = { 0 };
