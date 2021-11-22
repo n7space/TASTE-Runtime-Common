8b6b4ac9f92b0d43ea1194be15e35f6aaec0b9e4
jrachucki <jrachucki@n7space.com>
2021-11-22 14:25:10 +0100

    Escaper tests added

---
+++ tests/Escaper/EscaperTests.cpp
@@ -0,0 +1,229 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+#include <Escaper.h>
+}
+
+void* bus_to_driver_private_data[16];
+void* bus_to_driver_send_function[16];
+void* interface_to_deliver_function[16];
+
+TEST_GROUP(Escaper)
+{
+    static constexpr uint8_t NO_SPECIAL_CHARS1[]{ "HELLO" };
+    static constexpr uint8_t NO_SPECIAL_CHARS2[] = { "Goodbye!" };
+    static constexpr uint8_t ONLY_START_BYTES[] = { START_BYTE, START_BYTE, START_BYTE, START_BYTE,
+                                                    START_BYTE, START_BYTE, START_BYTE };
+    static constexpr uint8_t ONLY_STOP_BYTES[] = { STOP_BYTE, STOP_BYTE, STOP_BYTE, STOP_BYTE, STOP_BYTE };
+    static constexpr uint8_t ONLY_ESCAPE_BYTES[] = { ESCAPE_BYTE, ESCAPE_BYTE, ESCAPE_BYTE,
+                                                     ESCAPE_BYTE, ESCAPE_BYTE, ESCAPE_BYTE };
+
+    static constexpr int RANDOM_CHAR_STRING_LENGTH1{ 14 };
+    static constexpr int RANDOM_CHAR_STRING_LENGTH2{ 16 };
+
+    uint8_t RANDOM_CHARS_WITH_SPECIAL_BYTES1[RANDOM_CHAR_STRING_LENGTH1];
+    uint8_t RANDOM_CHARS_WITH_SPECIAL_BYTES2[RANDOM_CHAR_STRING_LENGTH2];
+
+    Escaper* escaper{ nullptr };
+
+    void setup()
+    {
+        // always create "fresh" structure before test
+        escaper = new Escaper;
+    }
+
+    void teardown() { delete escaper; }
+
+    void VerifyEncoding(const Escaper* const escaper, size_t encodedLength)
+    {
+        BYTES_EQUAL_TEXT(START_BYTE, escaper->m_send_packet_buffer[0], "No START_BYTE");
+        BYTES_EQUAL_TEXT(STOP_BYTE, escaper->m_send_packet_buffer[encodedLength - 1], "NO STOP_BYTE");
+
+        for(uint8_t j = 1; j < encodedLength - 1; j++) {
+            switch(escaper->m_send_packet_buffer[j]) {
+                case START_BYTE:
+                    FAIL("!!!! Non-escaped START_BYTE !!!! \n\r");
+                    break;
+                case STOP_BYTE:
+                    FAIL("!!!! Non-escaped START_BYTE !!!! \n\r");
+                    break;
+                case ESCAPE_BYTE:
+                    switch(escaper->m_send_packet_buffer[j + 1]) {
+                        case START_BYTE:
+                        case STOP_BYTE:
+                        case ESCAPE_BYTE:
+                            j++;
+                            break;
+                        default:
+                            FAIL("!!!! Non-escaped ESCAPE_BYTE !!!!");
+                            break;
+                    }
+            }
+        }
+    }
+
+    size_t SetupParsing(Escaper* const escaper, const uint8_t* const buffer, int size)
+    {
+        size_t index{ 0 }, encodedLength;
+        Escaper_init_encode(escaper);
+        encodedLength = Escaper_encode_packet(escaper, buffer, size, &index);
+        VerifyEncoding(escaper, encodedLength);
+
+        return encodedLength;
+    }
+
+    void VerifyParsing(const Escaper* const escaper, const uint8_t* const buffer, int size)
+    {
+        for(int j = 0; (j < size && j < sizeof(escaper->m_recv_packet_buffer)); j++) {
+            BYTES_EQUAL(buffer[j], escaper->m_recv_packet_buffer[j]);
+        }
+    }
+
+    void GenerateRandTestCase(uint8_t * buffer, int size)
+    {
+        for(int i = 0; i < size; i++) {
+            buffer[i] = rand() % ESCAPE_BYTE + 1; /// Generate random "non-special" byte
+        }
+
+        /// In random places, put bytes that need to be escaped
+        static constexpr int NUMBER_OF_START_BYTES{ 2 };
+        static constexpr int NUMBER_OF_STOP_BYTES{ 3 };
+        static constexpr int NUMBER_OF_ESCAPE_BYTES{ 1 };
+
+        for(int i = 0; i < NUMBER_OF_START_BYTES; i++) {
+            buffer[rand() % size] = START_BYTE;
+        }
+
+        for(int i = 0; i < NUMBER_OF_STOP_BYTES; i++) {
+            buffer[rand() % size] = STOP_BYTE;
+        }
+
+        for(int i = 0; i < NUMBER_OF_ESCAPE_BYTES; i++) {
+            buffer[rand() % size] = ESCAPE_BYTE;
+        }
+    }
+};
+
+TEST(Escaper, encodingNoSpecialBytes1)
+{
+    size_t index{ 0 }, encodedLength;
+
+    Escaper_init_encode(escaper);
+    encodedLength = Escaper_encode_packet(escaper, NO_SPECIAL_CHARS1, sizeof(NO_SPECIAL_CHARS1), &index);
+    VerifyEncoding(escaper, encodedLength);
+};
+
+TEST(Escaper, encodingNoSpecialBytes2)
+{
+    size_t index{ 0 }, encodedLength;
+
+    Escaper_init_encode(escaper);
+    encodedLength = Escaper_encode_packet(escaper, NO_SPECIAL_CHARS2, sizeof(NO_SPECIAL_CHARS2), &index);
+    VerifyEncoding(escaper, encodedLength);
+};
+
+TEST(Escaper, encodingOnlySTART_BYTES)
+{
+    size_t index{ 0 }, encodedLength;
+
+    Escaper_init_encode(escaper);
+    encodedLength = Escaper_encode_packet(escaper, ONLY_START_BYTES, sizeof(ONLY_START_BYTES), &index);
+    VerifyEncoding(escaper, encodedLength);
+};
+
+TEST(Escaper, encodingOnlySTOP_BYTES)
+{
+    size_t index{ 0 }, encodedLength;
+
+    Escaper_init_encode(escaper);
+    encodedLength = Escaper_encode_packet(escaper, ONLY_STOP_BYTES, sizeof(ONLY_STOP_BYTES), &index);
+    VerifyEncoding(escaper, encodedLength);
+};
+
+TEST(Escaper, encodingOnlyEscape_BYTES)
+{
+    size_t index{ 0 }, encodedLength;
+
+    Escaper_init_encode(escaper);
+    encodedLength = Escaper_encode_packet(escaper, ONLY_ESCAPE_BYTES, sizeof(ONLY_ESCAPE_BYTES), &index);
+    VerifyEncoding(escaper, encodedLength);
+};
+
+TEST(Escaper, encodingRandChar1)
+{
+    size_t index{ 0 }, encodedLength;
+    GenerateRandTestCase(RANDOM_CHARS_WITH_SPECIAL_BYTES1, RANDOM_CHAR_STRING_LENGTH1);
+    Escaper_init_encode(escaper);
+    encodedLength = Escaper_encode_packet(
+            escaper, RANDOM_CHARS_WITH_SPECIAL_BYTES1, sizeof(RANDOM_CHARS_WITH_SPECIAL_BYTES1), &index);
+    VerifyEncoding(escaper, encodedLength);
+};
+
+TEST(Escaper, encodingRandChar2)
+{
+    size_t index{ 0 }, encodedLength;
+    GenerateRandTestCase(RANDOM_CHARS_WITH_SPECIAL_BYTES2, RANDOM_CHAR_STRING_LENGTH2);
+    Escaper_init_encode(escaper);
+    encodedLength = Escaper_encode_packet(
+            escaper, RANDOM_CHARS_WITH_SPECIAL_BYTES2, sizeof(RANDOM_CHARS_WITH_SPECIAL_BYTES2), &index);
+    VerifyEncoding(escaper, encodedLength);
+};
+
+TEST(Escaper, parsingNoSpecialBytes1)
+{
+    size_t encodedLength;
+    encodedLength = SetupParsing(escaper, NO_SPECIAL_CHARS1, sizeof(NO_SPECIAL_CHARS1));
+    Escaper_parse_recv_buffer(escaper, escaper->m_send_packet_buffer, encodedLength);
+    VerifyParsing(escaper, NO_SPECIAL_CHARS1, sizeof(NO_SPECIAL_CHARS1));
+};
+
+TEST(Escaper, parsingNoSpecialBytes2)
+{
+    size_t encodedLength;
+    encodedLength = SetupParsing(escaper, NO_SPECIAL_CHARS2, sizeof(NO_SPECIAL_CHARS2));
+    Escaper_parse_recv_buffer(escaper, escaper->m_send_packet_buffer, encodedLength);
+    VerifyParsing(escaper, NO_SPECIAL_CHARS2, sizeof(NO_SPECIAL_CHARS2));
+};
+
+TEST(Escaper, parsingOnlySTART_BYTES)
+{
+    size_t encodedLength;
+    encodedLength = SetupParsing(escaper, ONLY_START_BYTES, sizeof(ONLY_START_BYTES));
+    Escaper_parse_recv_buffer(escaper, escaper->m_send_packet_buffer, encodedLength);
+    VerifyParsing(escaper, ONLY_START_BYTES, sizeof(ONLY_START_BYTES));
+};
+
+TEST(Escaper, parsingOnlySTOP_BYTES)
+{
+    size_t encodedLength;
+    encodedLength = SetupParsing(escaper, ONLY_STOP_BYTES, sizeof(ONLY_STOP_BYTES));
+    Escaper_parse_recv_buffer(escaper, escaper->m_send_packet_buffer, encodedLength);
+    VerifyParsing(escaper, ONLY_STOP_BYTES, sizeof(ONLY_STOP_BYTES));
+};
+
+TEST(Escaper, parsingOnlyEscape_BYTES)
+{
+    size_t encodedLength;
+    encodedLength = SetupParsing(escaper, ONLY_ESCAPE_BYTES, sizeof(ONLY_ESCAPE_BYTES));
+    Escaper_parse_recv_buffer(escaper, escaper->m_send_packet_buffer, encodedLength);
+    VerifyParsing(escaper, ONLY_ESCAPE_BYTES, sizeof(ONLY_ESCAPE_BYTES));
+};
+
+TEST(Escaper, parsingRandChar1)
+{
+    size_t encodedLength;
+    GenerateRandTestCase(RANDOM_CHARS_WITH_SPECIAL_BYTES1, RANDOM_CHAR_STRING_LENGTH1);
+    encodedLength = SetupParsing(escaper, RANDOM_CHARS_WITH_SPECIAL_BYTES1, sizeof(RANDOM_CHARS_WITH_SPECIAL_BYTES1));
+    Escaper_parse_recv_buffer(escaper, escaper->m_send_packet_buffer, encodedLength);
+    VerifyParsing(escaper, RANDOM_CHARS_WITH_SPECIAL_BYTES1, RANDOM_CHAR_STRING_LENGTH1);
+};
+
+TEST(Escaper, parsingRandChar2)
+{
+    size_t encodedLength;
+    GenerateRandTestCase(RANDOM_CHARS_WITH_SPECIAL_BYTES2, RANDOM_CHAR_STRING_LENGTH2);
+    encodedLength = SetupParsing(escaper, RANDOM_CHARS_WITH_SPECIAL_BYTES2, sizeof(RANDOM_CHARS_WITH_SPECIAL_BYTES2));
+    Escaper_parse_recv_buffer(escaper, escaper->m_send_packet_buffer, encodedLength);
+    VerifyParsing(escaper, RANDOM_CHARS_WITH_SPECIAL_BYTES2, RANDOM_CHAR_STRING_LENGTH2);
+};
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,7 @@
 #include "CppUTest/CommandLineTestRunner.h"
 
 IMPORT_TEST_GROUP(Packetizer);
+IMPORT_TEST_GROUP(Escaper);
 
 int
 main(int argc, char** argv)
